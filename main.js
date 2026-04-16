d5d89cd198254022f25e35f61e58204f45f63916
T Play <210236050+tplaylive@users.noreply.github.com>
2026-04-16 19:58:08 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+[
+  {
+    "name": "BBC News",
+    "sources": [
+      {
+        "name": "UK",
+        "url": "https://tplay.live/out/news/bbc-news/index.m3u8"
+      },
+      {
+        "name": "Asia",
+        "url": "https://autumn-shape-b04a.soft-disk-1347.workers.dev/?url=https://cdn4.skygo.mn/live/disk1/BBC_News/HLSv3-FTA/BBC_News.m3u8"
+      }
+    ],
+    "category": ["News", "English", "UK"],
+    "description": "World News"
+  },
+  {
+    "name": "CNN",
+    "sources": [
+      {
+        "name": "USA",
+        "url": "https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cnnhd.mpd",
+        "type": "dash"
+      }
+    ],
+    "category": ["News", "English", "USA"],
+    "description": "World News"
+  },
+  {
+    "name": "Al Jazeera",
+    "sources": [
+      {
+        "name": "Qatar",
+        "url": "https://live-hls-web-aje-fa.thehlive.com/AJE/index.m3u8"
+      }
+    ],
+    "category": ["News", "English", "Qatar"],
+    "description": "World News"
+  }
+]

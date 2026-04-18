c967a2b5ae1a8743c2985f8699e84c2373d1562a
T Play <210236050+tplaylive@users.noreply.github.com>
2026-04-18 13:42:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,9 @@
         "url": "https://autumn-shape-b04a.soft-disk-1347.workers.dev/?url=https://cdn4.skygo.mn/live/disk1/BBC_News/HLSv3-FTA/BBC_News.m3u8"
       }
     ],
-    "category": ["News", "English", "UK"],
+    "img": "https://i.postimg.cc/s2679ZP4/bbc.jpg",
+    "category": ["News", "English", "USA"],
+    "package": ["150", "250", "500"],
     "description": "World News"
   },
   {
@@ -20,21 +22,16 @@
       {
         "name": "USA",
         "url": "https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cnnhd.mpd",
-        "type": "dash"
+        "type": "dash",
+        "drm": {
+          "kid": "900c43f0e02742dd854148b7a75abbec",
+          "key": "da315cca7f2902b4de23199718ed7e90"
+        }
       }
     ],
+    "img": "https://play-lh.googleusercontent.com/375NW5yL8owK_hW9igW9sh-YJbda9ZcygpDXuVvK_R7l-yJp-fuhb4qvUw_FE4XW4ms",
     "category": ["News", "English", "USA"],
-    "description": "World News"
-  },
-  {
-    "name": "Al Jazeera",
-    "sources": [
-      {
-        "name": "Qatar",
-        "url": "https://live-hls-web-aje-fa.thehlive.com/AJE/index.m3u8"
-      }
-    ],
-    "category": ["News", "English", "Qatar"],
+    "package": ["150", "250", "500"],
     "description": "World News"
   }
 ]
